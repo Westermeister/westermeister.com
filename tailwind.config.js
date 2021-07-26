eecd710e7abef7a381e538b7e7d881be687eb57b
Westermeister <westermeister12@gmail.com>
2021-07-26 00:00:09 -0700

    Add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: [
+    './frontend/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'abrilfatface': ['Abril Fatface', 'cursive'],
+      'inter': ['Inter', 'sans-serif'],
+      'lora': ['Lora', 'serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose
+    }
+  },
+  variants: {
+    extend: {
+      scale: ['active'],
+      textOpacity: ['active'],
+      opacity: ['active'],
+      borderOpacity: ['active'],
+      textDecoration: ['active'],
+    }
+  },
+  plugins: []
+}
